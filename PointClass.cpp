653e6f3b5bc33efa34622e030312b0b5ee98baed
w01164264 <seanbigler@mail.weber.edu>
2017-09-21 13:15:37 -0600

    Created PointClass file

---
+++ PointClass.cpp
@@ -0,0 +1,44 @@
+// File: PointClass
+// Created by Sean Bigler on 9/21/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+class Point
+{
+private:
+    float x;        // x-coordinate
+    float y;        // y-coordinate
+public:
+    // Constructor
+    Point():x(0.0), y(0.0)
+    {}
+    // Methods
+    void showPoint()
+    {
+        cout << "The point is (" << x << ", " << y << ")" << endl;
+    }
+    void updatePoint(float xnew, float ynew)
+    {
+        x = xnew;
+        y = ynew;
+    }
+};
+
+// Prototypes
+
+
+// Main Program Program
+int main()
+{
+    Point p1;
+    p1.showPoint();     // Display values
+    p1.updatePoint(2.5, 3.9);
+    p1.showPoint();
+
+    return 0;
+}
+
+// Function Definitions
